f78a1cf452b43e740eef8261d7d5d0042f5d301c
estelle0500 <estelle0500@gmail.com>
2021-04-14 11:02:05 -0500

    Link view memories and add memory screens

--- app/src/main/java/com/example/imoodmeter/ViewMemoriesActivity.java
+++ app/src/main/java/com/example/imoodmeter/ViewMemoriesActivity.java
@@ -4,23 +4,45 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.imoodmeter.adapter.MemoryAdapter;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-public class ViewMemoriesActivity extends AppCompatActivity {
+public class ViewMemoriesActivity extends AppCompatActivity implements View.OnClickListener {
+    MemoryAdapter mMemoriesAdapter = new MemoryAdapter();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.view_memories);
 
-        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        // Put the memories in the recycler view
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setAdapter(mMemoriesAdapter);
 
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
 
-        MemoryAdapter memoriesAdapter = new MemoryAdapter();
-        recyclerView.setAdapter(memoriesAdapter);
+        // Link to add memory screen
+        FloatingActionButton addButton = findViewById(R.id.add_button);
+        addButton.setOnClickListener(this);
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        mMemoriesAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.add_button) {
+            Intent intent = new Intent(this, AddMemoryActivity.class);
+            startActivity(intent);
+        }
     }
 }
