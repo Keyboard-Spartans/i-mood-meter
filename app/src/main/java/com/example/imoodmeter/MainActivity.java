fb2825b74266a2af8861233cf9d6ea318293a402
Jacobvp2 <jacobvp2@illinois.edu>
2021-04-22 13:12:57 -0500

    Updated Calendar View With Fragment

--- app/src/main/java/com/example/imoodmeter/MainActivity.java
+++ app/src/main/java/com/example/imoodmeter/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.imoodmeter;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +14,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TabLayout tabLayout = findViewById(R.id.tabBar);
+        TabItem tabCalendar = findViewById(R.id.tabCalendar);
+        TabItem tabChart = findViewById(R.id.tabChart);
+
+        ViewPager viewPager = findViewById(R.id.ViewPager);
+
+        PagerAdapter pagerAdapter = new PagerAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
+
+        viewPager.setAdapter(pagerAdapter);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
     }
 
 
