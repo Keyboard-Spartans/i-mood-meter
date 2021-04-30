f8e1f13be57db743d4ff6d7d7079fa453f5b013a
edwardpassagi <edward.passagi@gmail.com>
2021-04-30 01:09:13 -0500

    fully working calendar, minor bug on task change

--- app/src/main/java/com/example/imoodmeter/CalendarFragment.java
+++ app/src/main/java/com/example/imoodmeter/CalendarFragment.java
@@ -51,12 +51,6 @@ public class CalendarFragment extends Fragment {
         final View view = inflater.inflate(R.layout.fragment_calendar, container, false);
 
         customCalendar = view.findViewById(R.id.customCalendar);
-//
-//        String[] arr = {"2021-04-10", "2021-04-10", "2021-04-15", "2021-04-16", "2021-04-25"};
-//        for (int i = 0; i < arr.length; i++) {
-//            int eventCount = 3;
-//            customCalendar.addAnEvent(arr[i], eventCount, getEventDataList(i==1? 2 : eventCount));
-//        }
 
         List<MoodModel> moods = MoodController.getMoods();
         for (MoodModel mood: moods) {
@@ -73,16 +67,13 @@ public class CalendarFragment extends Fragment {
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     public ArrayList<EventData> getEventsOnDate(String date) {
-        Log.i("GET_EVENTS_ON_DATE", date);
         ArrayList<EventData> eventDataList = new ArrayList();
 
         List<MoodModel> moods = MoodController.getMoods();
 
         for(MoodModel mood: moods) {
             String curDate = mood.getMoodTimeRecorded().format(formatter);
-            Log.e("COMPARISON", curDate + " vs " + date);
             if (curDate.equals(date)) {
-                Log.w("event match", "adding events on " + curDate + moodUtils.floatToMoodConverter(mood.getMoodValue()) + mood.getMoodDescription());
                 EventData dateData = new EventData();
                 ArrayList<dataAboutDate> dataAboutDates = new ArrayList();
 
@@ -97,23 +88,6 @@ public class CalendarFragment extends Fragment {
                 eventDataList.add(dateData);
             }
         }
-
-//        for (int i = 0; i < count; i++) {
-//            EventData dateData = new EventData();
-//            ArrayList<dataAboutDate> dataAboutDates = new ArrayList();
-//
-//            dateData.setSection(" ");
-//            dataAboutDate dataAboutDate = new dataAboutDate();
-//
-////            int index = new Random().nextInt(CalendarUtils.getEVENTS().length);
-//
-//            dataAboutDate.setTitle("Mood Here");
-//            dataAboutDate.setSubject("Optional Description From Mood Model Here");
-//            dataAboutDates.add(dataAboutDate);
-//
-//            dateData.setData(dataAboutDates);
-//            eventDataList.add(dateData);
-//        }
 
         return eventDataList;
     }
